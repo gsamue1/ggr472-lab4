703570ee70f4f161a124b14fa734b5c3f1078dff
gsamue1 <122768087+gsamue1@users.noreply.github.com>
2023-03-20 00:49:11 -0400

    Building Bounding Box and Hexgrids

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NhbXVlbC11b2Z0IiwiYSI6ImNsY3lieDA3MjJjNnAzc
 const map = new mapboxgl.Map({
     container: 'map', // div container ID for map
     style: 'mapbox://styles/gsamuel-uoft/cle4l4pr5001t01ljheblcl08', // Link to mapbox style URL
-    center: [-79.39, 43.65], // starting position [longitude, latitude]
-    zoom: 12, // starting zoom
+    center: [-79.39, 43.67], // starting position [longitude, latitude]
+    zoom: 10, // starting zoom
 });
 
 /*--------------------------------------------------------------------
-Step 2: VIEW GEOJSON POINT DATA ON MAP
+Setting Up Collisions Geojson Variable
 --------------------------------------------------------------------*/
+//Empty variable to store Collision Data Responses from Fetch Function 
 let collisgeojson;
 
 // Fetch GeoJSON from URL and store response
-fetch('')
+fetch('https://raw.githubusercontent.com/gsamue1/ggr472-lab4/main/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
         console.log(response); //Check response in console
@@ -31,27 +32,112 @@ fetch('')
     });
 
 
-//HINT: Create an empty variable
-//      Use the fetch method to access the GeoJSON from your online repository
-//      Convert the response to JSON format and then store the response in your new variable
-
-
-
 /*--------------------------------------------------------------------
-    Step 3: CREATE BOUNDING BOX AND HEXGRID
+Building Bounding Box and Hexgrid
 --------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data then store as a feature collection variable
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
 
+map.on('load', () => {
 
+// Building Bounding Box and Rescaling to Appropriate Borders
+    let bboxgeojson;
+    let bbox = turf.envelope(collisgeojson);
+    let bboxscaled = turf.transformScale(bbox, 1.05)
+
+    bboxgeojson = {
+        "type": "FeatureCollection",
+        "features": [bboxscaled]
+    }
+
+//Finding Coordinates for Bounding Box Vertices
+    console.log(bboxscaled)
+    console.log(bboxscaled.geometry.coordinates)
+
+//Creating Hexgrid
+let bboxcoords =  [bboxscaled.geometry.coordinates[0][0][0],
+                   bboxscaled.geometry.coordinates[0][0][1],
+                   bboxscaled.geometry.coordinates[0][2][0],
+                   bboxscaled.geometry.coordinates[0][2][1],];
+let hexgeojson = turf.hexGrid(bboxcoords, 0.5, {units: 'kilometers'});
+
+//MAPPINF COLLISION POINTS - Add datasource using GeoJSON variable
+    map.addSource('collisions', {
+        type: 'geojson',
+        data: collisgeojson
+    });
+
+//MAPPING COLLISION POINTS - Set style for when new points are added to the data source
+    map.addLayer({
+        'id': 'collisions-points',
+        'type': 'circle',
+        'source': 'collisions',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'blue'
+        }
+    });
+
+//MAPPING BOUNDING BOX - Add datasource using GeoJSON variable
+    map.addSource('bbox-collisions', {
+        type: 'geojson',
+        data: bboxgeojson
+    });
+
+//MAPPING BOUNDING BBOX - Set style for when new points are added to the data source
+    map.addLayer({
+        'id': 'collisions-bbox-polygon',
+        'type': 'fill',
+        'source': 'bbox-collisions',
+        'paint': {
+            'fill-color': 'green',
+            'fill-opacity': 0.3,
+            'fill-outline-color': 'green'
+        },
+    });
+
+// //MAPPING HEXGRID - Add datasource using GeoJSON variable
+//       map.addSource('hexgrid-to', {
+//         type: 'geojson',
+//         data: hexgeojson
+//     });
+
+// //MAPPING HEXGRID - Set style for when new points are added to the data source
+//     map.addLayer({
+//         'id': 'hexgrid-toronto',
+//         'type': 'fill',
+//         'source': 'hexgrid-to',
+//         'paint': {
+//             'fill-opacity': 0.5,
+//             'fill-outline-color': 'blue'
+//         }
+//     });
 
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
 --------------------------------------------------------------------*/
+
+// NEEDS TO BE CONTINUED CODE INCOMPLETE AND NOT FUNCTIONAL 
+let collisionhex = turf.collect(hexgeojson, collisgeojson, '_id', 'values')
 //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
 //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
+
+
+//MAPPING AGREGGATED HEXGRID - Add datasource using GeoJSON variable
+map.addSource('hexgrid-collisions', {
+    type: 'geojson',
+    data: collisionhex
+});
+
+//MAPPING MAPPING AGGREGATED HEXGRID - Set style for when new points are added to the data source
+map.addLayer({
+    'id': 'hexcollisions',
+    'type': 'fill',
+    'source': 'hexgrid-collisions',
+    'paint': {
+        'fill-opacity': 0.5,
+        'fill-outline-color': 'blue'
+    }
+});
+});
 
 
 
@@ -64,5 +150,3 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The COUNT attribute
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
-
-
